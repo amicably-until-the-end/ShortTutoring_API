6e00fa5b576cd4a5175b909001e13323a9404865
w8385 <w8385@naver.com>
2023-07-09 22:09:36 +0900

    🔨 Edit request APIs

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,15 +1,19 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { CreateRequestDto } from './dto/create-request.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { v4 as uuid } from 'uuid';
 
 @ApiTags('Request')
-@Controller('requests')
+@Controller('request')
 export class RequestsController {
   constructor(private readonly requestsService: RequestsService) {}
 
-  @Post('create')
+  @Post()
+  @ApiOperation({
+    summary: '과외 요청 생성',
+    description: '`Student` 요청을 생성합니다.',
+  })
   create(@Body() createRequestDto: CreateRequestDto) {
     const request = {
       id: uuid(),
@@ -18,12 +22,20 @@ export class RequestsController {
     return this.requestsService.create(request);
   }
 
-  @Get('findAll')
+  @Get()
+  @ApiOperation({
+    summary: '모든 과외 요청 반환',
+    description: '현재 모집중인 모든 과외 요청을 반환합니다.',
+  })
   findAll() {
     return this.requestsService.findAll();
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: '특정 과외 요청 삭제',
+    description: '특정 과외 요청을 삭제합니다.',
+  })
   remove(@Param('id') id: string) {
     return this.requestsService
       .remove(id)
