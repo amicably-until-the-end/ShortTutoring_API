9164edf4b8f38a6a7cbb1df76bd2c8c8b6890d7e
w8385 <w8385@naver.com>
2023-07-12 11:22:49 +0900

    📜 Add Tutoring response form

--- src/tutoring/tutoring.controller.ts
+++ src/tutoring/tutoring.controller.ts
@@ -1,23 +1,27 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { TutoringService } from './tutoring.service';
-import { CreateTutoringDto } from './dto/create-tutoring.dto';
+import {
+  CreateTutoringDto,
+  Success_CreateTutoringDto,
+} from './dto/create-tutoring.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Tutoring')
 @Controller('tutoring')
 export class TutoringController {
-  constructor(private readonly tutoringsService: TutoringService) {}
+  constructor(private readonly tutoringService: TutoringService) {}
 
   @Post()
   @ApiResponse({
-    status: 200,
+    status: 201,
     description: '과외 생성 완료',
+    type: Success_CreateTutoringDto,
   })
   @ApiOperation({
     summary: '과외 생성',
     description: '`STUDENT`\n\n과외를 생성합니다.',
   })
   create(@Body() createTutoringDto: CreateTutoringDto) {
-    return this.tutoringsService.create(createTutoringDto);
+    return this.tutoringService.create(createTutoringDto);
   }
 }
