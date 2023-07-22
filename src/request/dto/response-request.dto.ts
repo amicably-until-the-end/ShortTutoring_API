79b3ae70d5ee9bdc9afd00fb342e3c7fc033750d
w8385 <w8385@naver.com>
2023-07-23 00:47:09 +0900

    🔍 GetRequests 내부 base64Image 변수 수정 > image

--- src/request/dto/response-request.dto.ts
+++ src/request/dto/response-request.dto.ts
@@ -61,8 +61,8 @@ export class Success_GetRequestsDto extends HttpResponseDto {
             createdAt: '2021-01-01T00:00:00.000Z',
           },
           problem: {
+            image: 'test-problem-image-url',
             description: 'test-description',
-            base64Image: 'test-base64-image',
             schoolLevel: 'test-school-level',
             schoolSubject: 'test-school-subject',
             schoolChapter: 'test-school-chapter',
