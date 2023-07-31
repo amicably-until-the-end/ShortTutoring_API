3109b7f82ef40385fa6a873a20dd25c203a3af88
w8385 <w8385@naver.com>
2023-07-31 23:36:19 +0900

    🧹 AccessToken.authorizationCode 삭제

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,8 +1,4 @@
 export class AccessToken {
-  static authorizationCode(headers: Headers) {
-    return { vendor: headers['vendor'], code: headers['code'] };
-  }
-
   static authorization(headers: Headers) {
     return { vendor: headers['vendor'], accessToken: headers['authorization'] };
   }
