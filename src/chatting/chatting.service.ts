b13b3643fd864d606bea89ae58cbdef8b73a015d
seongyunlee <private.seongyun@gmail.com>
2023-09-27 15:57:12 +0900

    chatting findOne 추가

--- src/chatting/chatting.service.ts
+++ src/chatting/chatting.service.ts
@@ -192,19 +192,23 @@ export class ChattingService {
   }
 
   async findOne(chattingRoomId: string, userId: string) {
-    const room = await this.chattingRepository.findOne(chattingRoomId);
-    if (room.studentId == userId || room.teacherId == userId) {
-      const userInfo = await this.userRepository.get(userId);
-      const questionInfo = await this.questionRepository.getInfo(
-        room.questionId,
-      );
-      const roomInfo = await this.makeChatItem(
-        { roomInfo: room, questionInfo },
-        userInfo,
-      );
-      return new Success('채팅방 정보를 불러왔습니다.', roomInfo);
-    } else {
-      return new Fail('해당 채팅방에 대한 권한이 없습니다.');
+    try {
+      const room = await this.chattingRepository.findOne(chattingRoomId);
+      if (room.studentId == userId || room.teacherId == userId) {
+        const userInfo = await this.userRepository.get(userId);
+        const questionInfo = await this.questionRepository.getInfo(
+          room.questionId,
+        );
+        const roomInfo = await this.makeChatItem(
+          { roomInfo: room, questionInfo },
+          userInfo,
+        );
+        return new Success('채팅방 정보를 불러왔습니다.', roomInfo);
+      } else {
+        return new Fail('해당 채팅방에 대한 권한이 없습니다.');
+      }
+    } catch (error) {
+      return new Fail('해당 채팅방 정보가 없습니다.');
     }
   }
 
