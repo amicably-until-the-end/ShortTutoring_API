12b751cbb6b1c30adea3c71d321459b55daf8757
w8385 <w8385@naver.com>
2023-10-06 16:56:35 +0900

    🔍 리뷰 모듈 삭제

--- src/review/review.module.ts
+++
@@ -1,9 +0,0 @@
-import { ReviewController } from './review.controller';
-import { ReviewService } from './review.service';
-import { Module } from '@nestjs/common';
-
-@Module({
-  controllers: [ReviewController],
-  providers: [ReviewService],
-})
-export class ReviewModule {}
