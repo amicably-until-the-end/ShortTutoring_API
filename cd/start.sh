ab28b5958b47f5e814b416146923c2ca905535c1
seongyunlee <private.seongyun@gmail.com>
2023-10-24 19:23:41 +0900

    add script for continuous deploy

--- cd/start.sh
+++ cd/start.sh
@@ -2,5 +2,6 @@ cd /home/ubuntu/api
 
 # 원래 node 프로세스 종료
 sudo kill -9 `ps -ef | grep 'node' | awk '{print $2}'`
-npm install
-nohup npm run start:dev >/home/ubuntu/logs 2>&1 </home/ubuntu/errors &
+sudo npm install
+nohup npm run start:dev >/home/ubuntu/log/logs 2>&1 </home/ubuntu/log/errors &
+sudo rm -rf "install"
