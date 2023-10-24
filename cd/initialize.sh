c6a0e4368a2cf48ef4908840a6e7d7882a5f6761
seongyunlee <private.seongyun@gmail.com>
2023-10-24 23:55:36 +0900

    add script for continuous deploy

--- cd/initialize.sh
+++ cd/initialize.sh
@@ -1,1 +1,1 @@
-if [ -d "/home/ubuntu/api" ]; then rm -Rf "/home/ubuntu/api"; fi
+if [ -d "/home/ubuntu/api" ]; then sudo find /home/ubuntu/api -type d -name "node_modules" -prune -o -exec sudo rm -rf {} \; fi
