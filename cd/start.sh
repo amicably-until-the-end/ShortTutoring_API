ec95d7a00ce2ed40df6e21c4282aed08b9bd5dae
seongyunlee <private.seongyun@gmail.com>
2023-10-25 03:37:53 +0900

    add script for continuous deploy

--- cd/start.sh
+++ cd/start.sh
@@ -4,6 +4,6 @@ cd /home/ubuntu/api
 sudo kill -9 `ps -ef | grep 'node' | awk '{print $2}'`
 sudo chmod -R 777 /home/ubuntu/api
 npm install
-echo "npm install" >> /home/ubuntu/start.log
+ls "-al /home/api" >> /home/ubuntu/start.log
 nohup "cross-env NODE_ENV=dev node /home/ubuntu/api/dist/main" 1>~/log/logs 2>&1  &
 echo "done start.sh"
